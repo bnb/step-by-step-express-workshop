0bedf21cf36ed3035276d873644a809bc97e4d58
Tierney Cyren <hello@bnb.im>
2019-04-19 20:16:57 -0400

    Change default response to reduce complexity

--- step-one/app.complete.js
+++ step-one/app.complete.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express! 👋')
+  response.send('Hello from XKCD-serv! 👋')
 })
 
 app.listen(port, () => console.log(`Our app is now listening on port ${port}!`))
